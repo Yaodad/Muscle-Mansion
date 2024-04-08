a63da624bae3de5d3892d15bbca81fdcfe2f58da
Yaodad <yaodadbarro@gmail.com>
2024-04-09 01:31:25 +0800

    Update map marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
 const Map = () => {
   const coordinates = [14.828201, 120.883354];
+  const markerIcon = new Icon({
+    iconUrl: '/mm-person.png',
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+    popupAnchor: [1, -52],
+  });
 
   return (
     <>
@@ -14,13 +21,14 @@ const Map = () => {
           height: '420px',
           backgroundColor: '#f5f5f5',
           marginTop: '90px',
+          zIndex: 0,
         }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={coordinates}>
+        <Marker position={coordinates} icon={markerIcon}>
           <Popup>Muscle Mansion's Location</Popup>
         </Marker>
       </MapContainer>
